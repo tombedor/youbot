cf31be991a7dc88ac31b8e83efd38d8fd4715596
Tom Bedor <tombedor@gmail.com>
2024-01-27 10:42:53 -0800

    restore old version of delegates

--- demos/delegates.py
+++ demos/delegates.py
@@ -3,36 +3,14 @@ from memgpt.config import MemGPTConfig
 from memgpt.data_types import User, AgentState
 from memgpt.metadata import MetadataStore
 from copy import deepcopy
-import os
-import discord
 
-memgpt_client = MemGPT(config=vars(MemGPTConfig.load()))
+TESTBOT_NAME = 'testbot'
 
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    print(message)
-    if message.author == client.user:
-        return
-    
-    tombot_id = next(a['name'] for a in memgpt_client.list_agents()['agents'] if a['name'] == 'tombot')
-    response = memgpt_client.user_message(
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-    else:
-        print('not responding')
+_config = MemGPTConfig.load()
 
 
+client =  MemGPT(config = _config, overwrite_config=False)
 
-client.run(os.getenv('DISCORD_TOKEN'))
+client.list_agents()
 
+agent_ids =[d['id'] for d in client.list_agents()['agents'] if d['name'] == TESTBOT_NAME]
