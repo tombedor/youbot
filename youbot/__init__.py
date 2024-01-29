7b431fff8be30caecf662572bf7ea2fb538c56ba
Tom Bedor <tombedor@gmail.com>
2024-01-29 20:00:04 +0000

    fix env vars

--- youbot/__init__.py
+++ youbot/__init__.py
@@ -2,14 +2,15 @@ import os
 from dotenv import load_dotenv
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+MEMGPT_CONFIG_FILE = os.path.join(os.path.dirname(ROOT_DIR), 'config', 'memgpt_config.yaml')
 
 load_dotenv()
 
-for env_var in ['OPENAI_API_KEY', 'GITHUB_TOKEN', 'POSTGRES_URL', 'MEMGPT_CONFIG_PATH']:
+for env_var in ['OPENAI_API_KEY', 'GITHUB_TOKEN', 'POSTGRES_URL']:
     if os.getenv(env_var) is None:
         raise KeyError(f"missing required env var: {env_var}")
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 POSTGRES_URL = os.getenv('POSTGRES_URL')
-MEMGPT_CONFIG_PATH = os.getenv('MEMGPT_CONFIG_PATH')
+os.environ['MEMGPT_CONFIG_PATH'] = MEMGPT_CONFIG_FILE
