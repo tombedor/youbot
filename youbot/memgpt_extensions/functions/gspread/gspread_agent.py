a82b9e3a47692d5b7aaef5eb514e0d69ff832c65
Tom Bedor <tombedor@gmail.com>
2024-02-03 16:32:09 -0800

    reorganize function files

--- youbot/memgpt_extensions/functions/gspread/gspread_agent.py
+++
@@ -1,9 +0,0 @@
-# IN PROGRESS
-
-import gspread
-
-# TODO: read creds from env var
-# gc = gspread.service_account()
-
-# Open a sheet from a spreadsheet in one go
-# wks = gc.open("Where is the money Lebowski?")
