3ff56a30812bc25738756ca2a04721379d9b7b6b
Tom Bedor <tombedor@gmail.com>
2024-02-12 21:34:10 -0800

    add email checking

--- youbot/memgpt_extensions/functions/gmail.py
+++ youbot/memgpt_extensions/functions/gmail.py
@@ -7,6 +7,8 @@ from youbot import GOOGLE_CREDS_PATH, GOOGLE_EMAIL, SECRETS_DIR
 from youbot.service.google_service import fetch_google_email
 
 
+gmail = Gmail(client_secret_file=GOOGLE_CREDS_PATH, creds_file=os.path.join(SECRETS_DIR, 'gmail_creds.json'))
+
 def send_email(self, subject: str, message: str) -> str:
     """Sends an email to the user's linked google email
 
@@ -18,9 +20,7 @@ def send_email(self, subject: str, message: str) -> str:
         str: Description of the result of the email sending attempt.
     """
 
-    email = fetch_google_email(self.agent_state.user_id)
-    
-    gmail = Gmail(client_secret_file=GOOGLE_CREDS_PATH, creds_file=os.path.join(SECRETS_DIR, 'gmail_creds.json'))
+    email = fetch_google_email(self.agent_state.user_id)    
 
     params = {
         "to": email,
@@ -31,4 +31,18 @@ def send_email(self, subject: str, message: str) -> str:
         "signature": True  # use my account signature
     }
     message = gmail.send_message(**params)
-    
+
+# TODO: move to job queue
+def respond_to_emails(self):
+    """Responds to emails in the user's inbox
+
+    Returns:
+        str: Description of the result of the email responding attempt.
+    """
+    email = fetch_google_email(self.agent_state.user_id)
+    messages = gmail.get_unread_inbox(query=f"from:{email} -filename:*ics")
+    for message in messages:
+        if message.sender != GOOGLE_EMAIL:
+            response = f"The following message was received via email. Respond to the email: {message.plain}"
+            message.mark_as_read()
+            return response
