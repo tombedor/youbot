c4808265cc848a050911c6da77d33cdaf64d1c6f
Tom Bedor <tombedor@gmail.com>
2024-03-03 09:32:14 +1300

    update install to symlink dirs rather than files

--- script/install.py
+++ script/install.py
@@ -4,6 +4,7 @@
 
 import logging
 import os
+import shutil
 
 from youbot import ROOT_DIR
 
@@ -13,20 +14,9 @@ memgpt_install_dir = os.path.expanduser("~/.memgpt")
 
 # flattens and symlinks python files within a dir
 def symlink_files(src_dir, dest_dir):
-    logging.info(f"symlinking files from {src_dir} to {dest_dir}")
-    # Ensure destination directory exists
-    os.makedirs(dest_dir, exist_ok=True)
-
-    for root, dirs, files in os.walk(src_dir):
-        for file in files:
-            src_file = os.path.join(root, file)
-            dest_path = os.path.join(dest_dir, file)
-
-            # if dest exists, rm it
-            if os.path.exists(dest_path):
-                os.remove(dest_path)
-            os.symlink(src_file, dest_path)
-            print(f"symlinked {src_file} to {dest_path}")
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+    os.symlink(src_dir, dest_dir)
 
 
 def copy_creds():
