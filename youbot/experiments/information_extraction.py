b8772fbfe76c65b634833785b94442e80d0138f6
Tom Bedor <tombedor@gmail.com>
2024-03-17 14:13:06 -0700

    info extrac

--- youbot/experiments/information_extraction.py
+++ youbot/experiments/information_extraction.py
@@ -12,3 +12,10 @@
 
 # https://github.com/predibase/examples/blob/main/relation_extraction/Fine_tune_LLMs_for_Relation_Extraction.ipynb
 # https://github.com/predibase/examples/blob/main/relation_extraction/Fine_tune_LLMs_for_Relation_Extraction.ipynb
+
+# https://github.com/explosion/projects/tree/v3/tutorials/nel_emerson
+# https://github.com/explosion/projects/tree/v3/tutorials
+# https://maartengr.github.io/BERTopic/getting_started/quickstart/quickstart.html
+# https://maartengr.github.io/BERTopic/getting_started/quickstart/quickstart.html#visualizations
+# https://huggingface.co/tasks/reinforcement-learning
+
