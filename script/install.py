837a9f484ef36d30f50b90f5c7edeaec5e739a50
Tom Bedor <tombedor@gmail.com>
2024-03-20 12:16:28 -0700

    wip ssh tunnel

--- script/install.py
+++ script/install.py
@@ -2,10 +2,13 @@
 
 # assumes "poetry install" has already been run
 
+import getpass
+import json
 import logging
 import os
 from pathlib import Path
 import shutil
+import subprocess
 
 from youbot import ROOT_DIR
 
@@ -33,6 +36,32 @@ key = {openai_key}
     with open(creds_path, "w") as file:
         file.write(creds_text)
 
+# assumes one host and one db, TODO do this with tags?
+def setup_database_ssh_tunnel():
+    subprocess.run("doctl auth init", shell=True, capture_output=True, text=True)
+    
+    
+    response = subprocess.run("doctl compute droplet list --output json", shell=True, capture_output=True, text=True)
+    network_infos = json.loads(response.stdout)[0]['networks']['v4']
+    public_ip = next(entry['ip_address'] for entry in network_infos if entry['type'] == 'public')
+    
+    response = subprocess.run("doctl databases list --output json", shell=True, capture_output=True, text=True)
+    db_connection_info = json.loads(response.stdout)[0]['private_connection']
+    host = db_connection_info['host']
+    port = db_connection_info['port']
+    
+    
+    subprocess.run(f"ssh -N -L {port}:{host}:{port} root@{public_ip}", shell=True, capture_output=True)
+    # replace background process
+    # TODO: use autossh
+    
+    
+    
+    
+    
+    
+    
+    
 
 if __name__ == "__main__":
     copy_creds()
@@ -41,6 +70,10 @@ if __name__ == "__main__":
         dest_dir = os.path.join(memgpt_install_dir, name)
 
         symlink_files(source_dir, dest_dir)
+    setup_database_ssh_tunnel()
+        
+    
+    
 
     os.system("git pull origin main")
     os.system("poetry update pymemgpt")
