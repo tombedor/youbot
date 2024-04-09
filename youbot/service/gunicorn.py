ed6c802dd83470909cd0006ec2a7b2bf43899263
Tom Bedor <tombedor@gmail.com>
2024-04-09 16:35:39 -0700

    add gunicorn and function handler

---
+++ youbot/service/gunicorn.py
@@ -0,0 +1,33 @@
+import multiprocessing
+
+# Server socket
+bind = '127.0.0.1:8000'
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+# Security
+limit_request_line = 4094
+limit_request_fields = 100
+limit_request_field_size = 8190
+
+# Server mechanics
+daemon = False
+# raw_env = [
+#     'DJANGO_SECRET_KEY=something',
+#     'SOME_OTHER_ENV=something',
+# ]
+
+# Logging
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+# Process naming
+proc_name = 'youbot'
