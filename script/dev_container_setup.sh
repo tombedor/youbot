cead1e30981e7c9ae7edd4cdb12a3d6dadc363b5
Tom Bedor <tombedor@gmail.com>
2024-04-10 20:16:31 +0000

    fix install

--- script/dev_container_setup.sh
+++ script/dev_container_setup.sh
@@ -16,17 +16,16 @@ sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo
 sudo apt-get update && sudo apt-get -y install postgresql-12
 
 # starting postgres
-sudo -u root sudo -u postgres /usr/lib/postgresql/12/bin/postgres -D /var/lib/postgresql/12/main -c config_file=/etc/postgresql/12/main/postgresql.conf
+sudo -u root sudo -u postgres nohup /usr/lib/postgresql/12/bin/postgres -D /var/lib/postgresql/12/main -c config_file=/etc/postgresql/12/main/postgresql.conf > /tmp/nohup.out &
 
 sudo -u root sudo -u postgres psql -c "CREATE USER youbot WITH PASSWORD 'youbot';"
 sudo -u root sudo -u postgres psql -c "CREATE DATABASE youbot;"
-sudo -u root sudo -u postgres psql -c "GRANT ALL PERMISSIONS ON DATABASE youbot TO youbot;"
+sudo -u root sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE youbot TO youbot;"
 
-export POSTGRES_URL="postgresql://youbot:youbot@localhost/youbot"
-export MEMGPT_CONFIG_PATH="/workspaces/youbot/config/memgpt_config"
+####### install dependencies #######
+poetry install
 
-alias youbot="poetry run memgpt run --agent youbot --persona sam_pov --human basic --first --debug"
-
+###### memgpt setup #######
 mkdir -p /home/$USER/.memgpt
 
 cat << EOF > /$HOME/.memgpt/credentials
@@ -35,5 +34,8 @@ auth_type = bearer_token
 key = $(echo $OPENAI_API_KEY)
 EOF
 
-####### install dependencies #######
-poetry install
+
+####### setup aliases #######
+export POSTGRES_URL="postgresql://youbot:youbot@localhost/youbot"
+export MEMGPT_CONFIG_PATH="/workspaces/youbot/config/memgpt_config"
+alias youbot="poetry run memgpt run --agent youbot --persona sam_pov --human basic --first --debug"
