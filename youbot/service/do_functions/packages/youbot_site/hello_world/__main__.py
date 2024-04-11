3606c6c1ad8bbe3b5abe21b82d27cbc63e928f0f
Tom Bedor <tombedor@gmail.com>
2024-04-11 15:51:01 -0700

    simplify hello world

--- youbot/service/do_functions/packages/youbot_site/hello_world/__main__.py
+++ youbot/service/do_functions/packages/youbot_site/hello_world/__main__.py
@@ -1,27 +1,5 @@
-import os
-import requests
-
-
 def main(event, context):
-    print(event)
-    print(dir(context))
-    # Fetch the IP from the environment variable
-    ip_address = "MY_IP"
-
-    print(f'IP address: {ip_address}')
-
-    # Create the URL, assuming the prefix is http
-    url = f'http://{ip_address}:8000/'
-    
-    print(url)
-
-    # Make the GET request, catch and print any request exception
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-
-        # Print the response if there's no exception
-        print(response.text)
-
-    except requests.exceptions.RequestException as err:
-        print(f'Request failed: {err}')
+    return {
+        "body": {"hello world!"},
+        "statusCode": 200,
+    }
