173f403454909c6b4622d2b4878ee2191a4326c7
Tom Bedor <tombedor@gmail.com>
2024-04-12 16:47:32 +0000

    fix dev container

--- script/dev_container_setup.sh
+++ script/dev_container_setup.sh
@@ -19,11 +19,8 @@ sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo
 sudo apt-get update && sudo apt-get -y install postgresql-12
 
 # starting postgres
+# this returns even thought the db isn't started yet, hacky bit is to just put the db commands lower in the script
 sudo -u root sudo -u postgres nohup /usr/lib/postgresql/12/bin/postgres -D /var/lib/postgresql/12/main -c config_file=/etc/postgresql/12/main/postgresql.conf > /tmp/nohup.out &
-
-sudo -u root sudo -u postgres psql -c "CREATE USER youbot WITH PASSWORD 'youbot';"
-sudo -u root sudo -u postgres psql -c "CREATE DATABASE youbot;"
-sudo -u root sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE youbot TO youbot;"
 
 ####### install dependencies #######
 poetry config virtualenvs.in-project true
@@ -56,6 +53,11 @@ if [ -n "$DIGITAL_OCEAN_TOKEN" ]; then
     doctl serverless connect
     popd
 fi
+
+sudo -u root sudo -u postgres psql -c "CREATE USER youbot WITH PASSWORD 'youbot';"
+sudo -u root sudo -u postgres psql -c "CREATE DATABASE youbot;"
+sudo -u root sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE youbot TO youbot;"
+
 
 ####### setup aliases #######
 
