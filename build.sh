12064bcd87abbbc4f42215b36ddeadac971110f1
Tom Bedor <tombedor@gmail.com>
2024-04-12 11:48:56 -0700

    add functions

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -ue
 pwd
 env
-mkdir -p $HOME/.memgpt/
+mkdir -p $HOME/.memgpt/functions
+cat << EOF > $HOME/.memgpt/credentials
+[openai]
+auth_type = bearer_token
+key = $(echo $OPENAI_API_KEY)
+EOF
+
+cp $HOME/youbot/memgpt_extensions/functions/*py $HOME/.memgpt/functions
+
 pip install -r requirements.txt
 python script/configure_memgpt.py
