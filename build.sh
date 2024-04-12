28ff362b2d19e14e1f0bffb2f0318b2c02dad662
Tom Bedor <tombedor@gmail.com>
2024-04-12 11:56:55 -0700

    adjust buildg

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -ue
 pwd
 env
+whomai
 
 pip install -r requirements.txt
 
 mkdir -p $HOME/.memgpt/functions
 
-cp $HOME/youbot/memgpt_extensions/functions/*py $HOME/.memgpt/functions
+cp /workspace/youbot/memgpt_extensions/functions/*py $HOME/.memgpt/functions
 
 cat << EOF > $HOME/.memgpt/credentials
 [openai]
