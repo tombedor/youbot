0e9f42ed03e158db3cc166b7c996441b89134132
Tom Bedor <tombedor@gmail.com>
2024-04-12 11:49:53 -0700

    adjust build

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -ue
 pwd
 env
+
+pip install -r requirements.txt
+
 mkdir -p $HOME/.memgpt/functions
+
+cp $HOME/youbot/memgpt_extensions/functions/*py $HOME/.memgpt/functions
+
 cat << EOF > $HOME/.memgpt/credentials
 [openai]
 auth_type = bearer_token
 key = $(echo $OPENAI_API_KEY)
 EOF
-
-cp $HOME/youbot/memgpt_extensions/functions/*py $HOME/.memgpt/functions
-
-pip install -r requirements.txt
-python script/configure_memgpt.py
