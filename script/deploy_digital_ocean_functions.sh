546670ab65ddbd99e5888c502eeb4cef6a7ba6d8
Tom Bedor <tombedor@gmail.com>
2024-04-11 18:23:39 -0700

    cleanup

--- script/deploy_digital_ocean_functions.sh
+++
@@ -1,3 +0,0 @@
-
-DROPLET_IP=$(doctl compute droplet list --tag-name youbot --format PublicIPv4 --no-header)
-doctl serverless deploy $0/../../youbot/service/do_functions
--- script/run_function_handler.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -ex
-
-gunicorn -c gunicorn.py function_handler:app
