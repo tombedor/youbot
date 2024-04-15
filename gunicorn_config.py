2a22e509d23cc41b574e4c8ea8d58805e9e1f9b1
Tom Bedor <tombedor@gmail.com>
2024-04-15 16:53:07 -0700

    rm unneeded gunicorn config

--- gunicorn_config.py
+++
@@ -1,9 +0,0 @@
-
-# workers = multiprocessing.cpu_count() * 2 + 1  # Number of worker processes
-workers = 4
-bind = '0.0.0.0:8080'                          # Specifies server binds to the desired IP and port
-timeout = 120                                  # Sets a timeouts for worker processes
-
-# Log settings
-# accesslog = 'access.log'  # where to log access
-# errorlog = 'error.log'    # where to log errors
