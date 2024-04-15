226cad2345bf783eb3cf511f0d0fe1299c0daa59
Tom Bedor <tombedor@gmail.com>
2024-04-15 14:57:20 -0700

    hardcode worker num

--- youbot/services/gunicorn.py
+++ youbot/services/gunicorn.py
@@ -1,6 +1,7 @@
 import multiprocessing
 
-workers = multiprocessing.cpu_count() * 2 + 1  # Number of worker processes
+# workers = multiprocessing.cpu_count() * 2 + 1  # Number of worker processes
+workers = 4
 bind = '0.0.0.0:8080'                          # Specifies server binds to the desired IP and port
 timeout = 120                                  # Sets a timeouts for worker processes
 
