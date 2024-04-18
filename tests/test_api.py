ceb6282b4007d449ee25d034226cb08c047a731d
Tom Bedor <tombedor@gmail.com>
2024-04-18 11:02:42 -0700

    cleanup, add tes

---
+++ tests/test_api.py
@@ -0,0 +1,16 @@
+import pytest
+from flask.testing import FlaskClient
+
+from youbot.api import app  # replace with the actual import for your Flask app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_routes(client: FlaskClient):
+    assert 'health' in app.url_map._rules_by_endpoint.keys()
+
+def test_health_route(client: FlaskClient):
+    response = client.get('/health')
+    assert response.status_code == 200
