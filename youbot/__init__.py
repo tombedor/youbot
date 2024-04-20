4433cb8d16b0f4ab7907c2d6508dce033c59fd76
Tom Bedor <tombedor@gmail.com>
2024-04-20 14:07:07 -0700

    fix env

--- youbot/__init__.py
+++ youbot/__init__.py
@@ -1,21 +1,10 @@
 import os
 import yaml
-import logging
-import sys
 
 
-# Expose memgpt config to MemGPT
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STORAGE_DIR = os.path.join(ROOT_DIR, "storage")
 
-os.environ["USER_FUNCTIONS_DIR"] = os.path.join(ROOT_DIR, "memgpt_extensions", "functions")
-
-
-def log_to_stdout():
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
-
-os.environ["MEMGPT_CONFIG_PATH"] = os.path.join(ROOT_DIR, "config", "memgpt_config")
 with open(os.path.join(ROOT_DIR, "config", "agents.yaml"), "r") as file:
     AGENTS_CONFIG = yaml.safe_load(file.read())
 
