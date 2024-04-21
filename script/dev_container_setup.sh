705ea368413cbc6683b1d7bcc3cc2c1ea1b94ca9
Tom Bedor <tombedor@gmail.com>
2024-04-21 03:00:13 +0000

    fix devcontainer, configure celery

--- script/dev_container_setup.sh
+++ script/dev_container_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-# note: expects OPENAI_API_TOKEN and DIGITAL_OCEAN_TOKEN to be available 
+# note: expects OPENAI_API_TOKEN and DIGITAL_OCEAN_TOKEN to be available
 # (for vscode container, use codespace secrets)
 
 ####### install postgres #######
@@ -10,7 +10,7 @@ set -ex
 sudo apt-get update && sudo apt-get install -y wget gnupg
 
 # adding PostgreSQL's official apt repository
-sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
 
 # importing the repository signing key
 sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
@@ -20,49 +20,17 @@ sudo apt-get update && sudo apt-get -y install postgresql-12
 
 # starting postgres
 # this returns even thought the db isn't started yet, hacky bit is to just put the db commands lower in the script
-sudo -u root sudo -u postgres nohup /usr/lib/postgresql/12/bin/postgres -D /var/lib/postgresql/12/main -c config_file=/etc/postgresql/12/main/postgresql.conf > /tmp/nohup.out &
+sudo -u root sudo -u postgres nohup /usr/lib/postgresql/12/bin/postgres -D /var/lib/postgresql/12/main -c config_file=/etc/postgresql/12/main/postgresql.conf >/tmp/nohup.out &
+
+### install redis ###
+
+sudo apt-get -y install redis-server
+sudo service redis-server start
 
 ####### install dependencies #######
 poetry config virtualenvs.in-project true
 poetry install
 
-###### memgpt setup #######
-mkdir -p /home/$USER/.memgpt
-
-cat << EOF > /$HOME/.memgpt/credentials
-[openai]
-auth_type = bearer_token
-key = $(echo $OPENAI_API_KEY)
-EOF
-
-
-##### install digital ocean tools #####
-# if DIGITAL_OCEAN_TOKEN is available, install and configure doctl
-if [ -n "$DIGITAL_OCEAN_TOKEN" ]; then
-    pushd .
-    cd $HOME
-    wget https://github.com/digitalocean/doctl/releases/download/v1.104.0/doctl-1.104.0-linux-amd64.tar.gz
-    tar xf ~/doctl-1.104.0-linux-amd64.tar.gz
-    sudo mv $HOME/doctl /usr/local/bin
-
-    doctl completion bash >> $HOME/.bash_profile
-    source $HOME/.bash_profile
-
-    doctl auth init -t $DIGITAL_OCEAN_TOKEN
-    doctl serverless install
-    doctl serverless connect
-    popd
-fi
-
 sudo -u root sudo -u postgres psql -c "CREATE USER youbot WITH PASSWORD 'youbot';"
 sudo -u root sudo -u postgres psql -c "CREATE DATABASE youbot;"
 sudo -u root sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE youbot TO youbot;"
-
-
-####### setup aliases #######
-
-cat << EOF >> $HOME/.bash_profile
-export DATABASE_URL="postgresql://youbot:youbot@localhost/youbot"
-export MEMGPT_CONFIG_PATH="/workspaces/youbot/config/memgpt_config"
-alias youbot="poetry run memgpt run --agent youbot --persona sam_pov --human basic --first --debug"
-EOF
---
+++ youbot/workers/celery_worker.py
@@ -0,0 +1,9 @@
+import os
+from youbot import app
+from celery import Celery
+
+app = Celery('youbot', broker=os.environ['REDIS_URL'], backend=os.environ['REDIS_URL'])
+
+@app.task
+def add(x, y):
+    return x + y
