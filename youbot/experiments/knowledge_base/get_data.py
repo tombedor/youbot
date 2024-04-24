eba686c7d49d081cbb3c738e2985c52d6fd1ac33
Tom Bedor <tombedor@gmail.com>
2024-04-23 17:55:56 -0700

    entity resolution
    add pipeline to classify entities and relationships between entities

---
+++ youbot/experiments/knowledge_base/get_data.py
@@ -0,0 +1,10 @@
+from pandas import DataFrame
+from youbot.store import Store
+
+
+docs = Store().get_archival_messages()  # [:MESSAGES_COUNT]
+
+df = DataFrame(docs)
+
+with open("msgs.pkl", "wb") as f:
+    df.to_pickle(f)
