41777c9acbc09d150961e250a5f923dad6dbf509
Tom Bedor <tombedor@gmail.com>
2024-04-24 14:50:45 -0700

    end to end example pipeline

--- youbot/experiments/kb_from_guide/kb_from_guide.py
+++ youbot/experiments/kb_from_guide/kb_from_guide.py
@@ -1,4 +1,4 @@
-# https://medium.com/mantisnlp/constructing-a-knowledge-base-with-spacy-and-spacy-llm-f65b50ea53d
+# https://medium.com/mantisnlp/constructing-a-knowledge-base-with-spacy-and-spacy-llm-f65b50ea534d
 # https://spacy.io/api/large-language-models#rel-v1
 
 import os
@@ -7,6 +7,7 @@ from time import sleep
 from spacy_llm.util import assemble
 import spacy
 
+DOCS_TO_INGEST = 10
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -21,11 +22,11 @@ except OSError:
 
 from datasets import load_dataset
 print('downloading data')
-docs = [d['text'] for d in load_dataset("argilla/news-summary")['train']][0:10] # type: ignore
+docs = [d['text'] for d in load_dataset("argilla/news-summary")['train']][0:DOCS_TO_INGEST] # type: ignore
 total = len(docs)
 
 docs_with_rels = []
-i = 0
+i = 1
 for doc in docs:
     print("processing", i, "of", total)
     try:
@@ -48,17 +49,35 @@ for doc in docs_with_rels:
     i += 1
 
 from pyvis.network import Network
-net = Network('500px', '500px', notebook=True, cdn_resources='in_line')
+net = Network('500px', '500px', cdn_resources='in_line') #cdn_resources='in_line')
 
 nodes = []
 for r in relation_data:
     if r[0] not in nodes:
-        nodes.append(r[0])
+        nodes.append(r[0].text)
     if r[2] not in nodes:
-        nodes.append(r[2])
-net.add_nodes(nodes)
-for r in relation_data:
-    net.add_edge(r[0], r[2], value=1, title=r[1])
+        nodes.append(r[2].text)
 
-net.show('nx.html')
+for node in nodes:
+    net.add_node(node, label=node)
+# net.add_nodes(nodes)
+for r in relation_data:
+    net.add_edge(r[0].text, r[2].text, value=1, title=r[1])
+
+net.save_graph('nx.html')
+
+
+# After your network is created
+print("Number of nodes:", net.num_nodes())
+print("Number of edges:", net.num_edges())
+
+# Print all nodes
+print("Nodes:")
+for node in net.nodes:
+    print(node)
+
+# Print all edges
+print("Edges:")
+for edge in net.edges:
+    print(edge)
     
